ea173cd1f36a609af32111b098b22aeb67c54ff0
jihyunkim0 <60856927+jihyunkim0@users.noreply.github.com>
2023-03-07 17:59:20 +0900

    feat: 목록 리스트 Backend에 요청하는 API 완료 [BOARDx]

--- KimJihyun/frontend/src/store/index.js
+++ KimJihyun/frontend/src/store/index.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import states from "@/store/states"
+import state from "@/store/states"
 import actions from "@/store/actions"
 import mutations from "@/store/mutations"
 import getters from "@/store/getters"
 
+Vue.use(Vuex)
 //원래 있던 중괄호는 import를 하면서 지움(지저분함 방지)
 // 현재 상태를 관리하는 이유는 세션과 쿠키를 vue에서 관리하기 위해서
-Vue.use(Vuex)
-
 export default new Vuex.Store({
-  states,
+  // 왜 states 안하고 state 하나요 ?
+  // states가 예약어이기 때문에 js 코드가 요상한 동작을 하게 됩니다.
+  state,
   actions,
   mutations,
   getters,
---
+++ KimJihyun/frontend/src/store/mutations.js
@@ -0,0 +1,10 @@
+import {
+    REQUEST_BOARD_LIST_TO_SPRING,
+} from './mutation-types'
+
+export default {
+    // states로 구성하면 안됨(예약어)
+    [REQUEST_BOARD_LIST_TO_SPRING] (state, passingData) {
+        state.boards = passingData
+    }
+}
